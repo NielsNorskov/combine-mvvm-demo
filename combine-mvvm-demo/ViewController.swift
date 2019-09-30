8bda99994d0cd42ced4ac102fec151b474ae9ba9
Niels Nørskov <nnorskov@gmail.com>
2019-09-30 17:44:33 +0200

    Initial commit

---
+++ combine-mvvm-demo/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  combine-mvvm-demo
+//
+//  Created by Niels Nørskov on 30/09/2019.
+//  Copyright © 2019 Niels Nørskov. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
